cc26dffa853b788341096891abb6a62ef105e718
Eduard-747 <88149958+Eduard-747@users.noreply.github.com>
2022-09-01 17:31:03 +0400

    fix code style

--- Redis/server.cpp
+++ Redis/server.cpp
@@ -8,7 +8,7 @@ Socket_reciver::Socket_reciver()
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd < 0)
     {
-        std::cerr <<  "Error opening socket\n";
+        std::cerr << "Error opening socket\n";
         std::exit(1);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -17,18 +17,18 @@ Socket_reciver::Socket_reciver()
     serv_addr.sin_port = htons(SERVER_PORT);
     if (bind(listenfd, (SA *) &serv_addr, sizeof(serv_addr)) < 0)
     {
-        std::cerr <<  "ERROR on binding\n";
+        std::cerr << "ERROR on binding\n";
         exit(1);
     }
     cout << "server ip : " << inet_ntoa(serv_addr.sin_addr) << std::endl;
     if (listen(listenfd, 5) < 0)
     {
-        std::cerr <<  "ERROR on listen\n";
+        std::cerr << "ERROR on listen\n";
         exit(1);
     }
 }
 
-void    Socket_reciver::accept_clients()
+void Socket_reciver::accept_clients()
 {
     while (1)
     {
@@ -55,11 +55,11 @@ int Socket_reciver::count_token(char *str, char token)
     return (count);    
 }
 
-char**    Socket_reciver::get_arr_from_command(char *command)
+char** Socket_reciver::get_arr_from_command(char *command)
 {
-    int     i;
-    int     arr_len;
-    char    **arr;
+    int i;
+    int arr_len;
+    char **arr;
 
     i = 0;
     arr_len = count_token(command, ' ') + 2;
